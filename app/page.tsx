36379b4e6d2a4a91f4afa5d5f92f87876af91e02
Alejandro Gala <alegala21@gmail.com>
2024-02-08 16:43:41 -0500

    final

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 const Header = () => {
   return (
     <header className="bg-black text-white px-5 py-3">
-      <div className="flex justify-center items-center w-full">
-        <Image src="/logo.png" alt="logo" width={60} height={60} />
+      <div className="flex justify-start pl-32 items-start w-full">
+        <Image src="/aegislogo.png" alt="logo" width={80} height={80} />
       </div>
     </header>
   );
@@ -22,51 +22,57 @@ const MainContent = () => {
           <div className=" h-full flex flex-col">
             <div className="h-3/5 w-full flex flex-row ">
               {/* TOP LEFT */}
-              <div className="w-2/3 h-full border-r-[1px] border-b-[1px] flex flex-col border-black pt-10 bg-[url('/background.png')]">
-                <div className="h-1/5 w-full text-black pl-10 flex items-center font-mono">
-                  <p className="text-6xl text font-semibold"> AEGIS </p>
+              <div className="w-2/5 h-full border-r-[1px] border-b-[1px] flex flex-col border-black pt-5 bg-[url('/background.png')]">
+                <div className="h-1/5 w-full text-black pl-5 flex items-center font-mono">
+                  <p className="text-[50px] text font-semibold"> aegis.art </p>
                 </div>
-                <div className="flex gap-y-3 w-3/5 pl-10 flex-col justify-between">
-                  <div className="h-1 w-3/4 rounded-3xl bg-slate-700"></div>
-                  <div className="h-1 w-3/5 rounded-3xl bg-slate-500"></div>
-                  <div className="h-1 w-3/4 rounded-3xl bg-slate-700"></div>
-                  <div className="h-1 w-3/5 rounded-3xl bg-slate-500"></div>
-                </div>
-
-                <div className="h-[16%] w-[55%] flex bg-black flex-row justify-between items-center mt-10 rounded-r-[64px] px-5 tracking-widest">
-                  <div className="h-1/6 w-[3%] bg-black border-2 border-white rounded-full"></div>
-                  <p className="text-lg text-white font-mono">
+                <div className="h-1/5 w-11/12 flex bg-black flex-row justify-between items-center mt-5 rounded-r-[64px] px-5 tracking-widest">
+                  <p className="text-md text-white font-mono py-5">
                     {" "}
-                    The New Way of Art
+                    a new home for art patrons
                   </p>
-                  <div className="h-1/6 w-[3%] bg-black border-2 border-white rounded-full"></div>
+                </div>
+                <div className="w-full h-full flex justify-center mt-10 p-4 font-semibold">
+                  <p>
+                    forget traditional commissions schemes, aegis turns digital
+                    art patronage into an experience:
+                    <br />
+                    <br />
+                    invest in a daring artistic project and get an exclusive
+                    insight into how it gets produced
+                  </p>
                 </div>
               </div>
               {/* TOP RIGHT */}
-              <div className="container w-1/3 h-full border-b-[1px] border-black bg-black flex flex-col justify-around items-center">
-                <div className="title-container w-2/3 my-4">
-                  <p className="title text-white text-5xl font-bold text-center font-mono">
-                    Patrons of Art
-                  </p>
-                </div>
-
-                <div className="description-container w-5/6 text-white mt-5 p-4 flex flex-col gap-y-2">
-                  <p className="description-text font-light text-xs text-center">
-                    Forget traditional commissions schemes, aegis turns digital
-                    art patronage into a comprehensive package
-                  </p>
-                  <p className="description-text font-light text-xs text-center">
-                    Patrons get to support daring artistic projects; in return,
-                    they get an exclusive insight into how the projects get
-                    produced.
-                  </p>
-                </div>
-
-                <div className="separator w-5/6 h-[1px] bg-white my-4"></div>
-
-                <div className="footer-container flex flex-col justify-center items-center w-5/6 h-3/5  p-4">
-                  <p className="text-sm font-bold  mt-8">
-                    Be part of the revolution
+              <div className="container w-3/5 h-full border-b-[1px] border-black bg-black flex flex-col justify-center items-center">
+                <div className="flex justify-center text-white font-semibold p-4 text-sm">
+                  <p>
+                    front-row seats to the production process
+                    <br />
+                    <br />
+                    no more one-off studio visits, presentations over Zoom and
+                    getting updates on your commissions via IG DMs. aegis bring
+                    patrons a new level of insight to the art production
+                    process: a token-gated content calendar filled with deep
+                    dive interviews on topics of your choosing, regular
+                    short-form video updates on the projects' many features,
+                    reading lists curated by the artist, an interactive
+                    moodboard and production roadmap, plus many more perks and
+                    benefits.
+                    <br />
+                    <br />
+                    investing in emerging artists has never been this intuitive
+                    <br />
+                    <br />
+                    at aegis, we focus on incentivizing digital art patrons to
+                    invest in an artist's work at the concept stage of an
+                    artwork's development. this is achieved through a novel sale
+                    structure: artists come up with a sum that reflects the
+                    production costs of the artwork, and they trade this sum
+                    with a patron in exchange for a select number of pieces from
+                    the future digital art collection. once an agreement is
+                    finalized, the aegis team focuses on promoting the
+                    collection via physical gallery shows and activations.
                   </p>
                 </div>
               </div>
@@ -75,14 +81,14 @@ const MainContent = () => {
             <div className="h-2/5 w-full flex flex-row ">
               {/* BOTTOM LEFT */}
               <div className="w-1/3 h-full border-r-[1px] flex flex-col border-black justify-between items-center p-4">
-                <p className="text-black font-mono text-2xl font-semibold">
+                <p className="text-black font-mono text-3xl font-semibold">
                   {" "}
                   Our Mission{" "}
                 </p>
 
-                <p className="text-black font-mono text-lg text-center font-semibold">
+                <p className="text-black font-mono text-xl text-center font-semibold">
                   {" "}
-                  bringing structure to the patronage process
+                  bringing structure to digital art patronage
                 </p>
                 <div className="flex h-1/5 w-3/4 justify-center items-center">
                   <div className=" h-full w-full rounded-3xl bg-black "></div>
@@ -94,7 +100,7 @@ const MainContent = () => {
                 <div className="flex flex-col justify-between  h-full py-4 px-8">
                   <div className="flex flex-row justify-between h-2/3 items-center ">
                     <p className="text-3xl text-center font-bold">
-                      Don't miss out on Aegis! Sign up for updates
+                      Don't miss out on aegis! Sign up for updates
                     </p>
                   </div>
 
